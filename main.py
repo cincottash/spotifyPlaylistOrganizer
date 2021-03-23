5b8eaa949e18c4386010ed328d7b68494f5f84c7
cincottash <cincotta@udel.edu>
2021-03-23 00:07:31 -0400

    Stuck on 50 playlist pull limit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from setup import *
+import requests
 
-def get_playlist_tracks(username, playlist_id):
+def getPlaylistTracks(username, playlist_id):
 	results = sp.user_playlist_tracks(username,playlist_id)
 	
 	tracks = results['items']
@@ -19,27 +20,94 @@ def getTrackGenres(track):
 
 	return trackGenres
 
+def getUserPlaylists(username):
+	playlistsData = sp.user_playlists(username)
+	playlistNames = []
+
+	for item in playlistsData['items']:
+		playlistNames.append(item['name'])
+
+	return playlistNames
+
+def checkPlaylistExists(genre, playlistNames):
+	if genre in playlistNames:
+		return True
+
+	return False
+
+#get playlist id from its name attribute
+def getPlaylistID(playlistName, username):
+
+	#THIS HAS A LIMIT OF 50 AND IS FUCKING EVERYTHIN UP
+	playlistsData = sp.user_playlists(username, limit=50)
+	print(len(playlistsData['items']))
+	for item in playlistsData['items']:
+		#print(item['name'])
+		if item['name'] == playlistName:
+			playlistID = item['id']
+			return playlistID
+	return None
+
+	
+
+def addSongToPlaylist(playlistID, trackID):
+	#user_playlist_add_tracks(user, playlist_id, tracks, position=None)
+	sp.user_playlist_add_tracks(userID, playlistID, [trackID], position=None)
+
+
+def makePlaylist(playlistName):
+	sp.user_playlist_create(userID, playlistName, public=True)
+
+
 def main():
-	masterTracks = get_playlist_tracks(userID, masterID)
+	masterTracks = getPlaylistTracks(clientID, masterID)
+	playlistNames = getUserPlaylists(userID)
+	print(playlistNames)
 
 	for track in masterTracks:
 		trackName = track['track']['name']
-		
+
+		trackID = track['track']['id']
+		print(trackName)
+
 		#List of all the genres an artist is associated with
 		trackGenres = getTrackGenres(track)
+		print(trackGenres)
+		print('\n')
+		#for each genre associated with an artists, check if a playlist with that name already exists
+		for genre in trackGenres:
+			if not checkPlaylistExists(genre, playlistNames):
+				print('playlist for genre {} not found, creating playlist {}'.format(genre, genre))
+				makePlaylist(genre)
+				playlistNames.append(genre)
+				playlistID = getPlaylistID(genre, userID)
+			else:
+				print('playlist found for genre {}'.format(genre))
+				playlistID = getPlaylistID(genre, userID)
+			
+			#Sometimes playlistID is none and idk why, it ends up skipping that song
+			if(playlistID == None):
+				print('Couldnt get playlistID of genre {}, skipping genre {}'.format(genre, genre))
+				print(playlistNames)
+				exit(0)
+				continue
+			else:
+				#check if song already exsists in playlist
+				playlistTracks = getPlaylistTracks(clientID, playlistID)
+				
+				trackExists = False
+				for playlistTrack in playlistTracks:
+					if playlistTrack['track']['id'] == trackID:
+						trackExists = True
+						print('Track {} already exists in playlist {}, skipping track\n'.format(trackName, genre))
+						break
+					trackExists = False
 
-		#print(trackGenres)
+				if not trackExists:
+					print('adding {} to {}\n'.format(trackName, genre))			
+					addSongToPlaylist(playlistID, trackID)
+		print('~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 
-		#TODO: Add track to a playlist according to each genre the artist is associated with
-
-		#check a playlist for each genre already exists, if it does, add this song to that playlist, if not, make the playlist and add the song
-
-
-
-
-
-		#only do the first track for testing
-		break
 
 if __name__ == '__main__':
 	main()
--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 import spotipy
+import json
 from spotipy.oauth2 import SpotifyClientCredentials
+import pprint 
+import spotipy.util as util
+pp = pprint.PrettyPrinter(indent=1)
+
 
 masterID = '5rIfKE22hnQfVojjYI6xre'
-userID = 'f0afa03a332f4f499904815cdee524fe'
+clientID = 'f0afa03a332f4f499904815cdee524fe'
+userID = 'ghq9ex4uk5hahtc36wlbmgn7f'
+clientSecret = '47030e7e93314cefbc42ef18d9e1e705'
 
-auth_manager = SpotifyClientCredentials('f0afa03a332f4f499904815cdee524fe', 'ff2fce807bb94c5cb9827eb913d9213c')
-sp = spotipy.Spotify(auth_manager=auth_manager)
+#client_credentials_manager = SpotifyClientCredentials(client_id=clientID, client_secret='47030e7e93314cefbc42ef18d9e1e705')
+#sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+username = userID
+token = util.prompt_for_user_token(
+    scope='playlist-modify-public playlist-modify-private', 
+    client_id=clientID, 
+    client_secret=clientSecret, 
+    redirect_uri="http://localhost:8888/callback"
+
+)
+sp = spotipy.Spotify(auth=token)
